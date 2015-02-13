252c359b5408a2e2660f89acb2fd45ebe34d3108
NEHA SUWAl <neha@jyaasa.com>
2015-02-13 14:57:23 +0545

    added action index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,14 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @user = current_user
   end
+
+  
+  private
+
+    def user_param
+      params.require(:users).permit(:first_name, :last_name, :login, :logout)
+    end
+
 end
