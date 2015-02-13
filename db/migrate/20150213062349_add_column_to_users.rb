6c06fc77b9fa452386827257a2c4d0314aeca74a
NEHA SUWAl <neha@jyaasa.com>
2015-02-13 13:13:24 +0545

    added new migration to add columns in users table

---
+++ db/migrate/20150213062349_add_column_to_users.rb
@@ -0,0 +1,11 @@
+class AddColumnToUsers < ActiveRecord::Migration
+  
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :admin, :boolean
+    add_column :users, :login, :datetime
+    add_column :users, :logout, :datetime
+  end
+
+end
