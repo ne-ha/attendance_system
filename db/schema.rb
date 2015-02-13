369fa89294c149c526c0eea794ac6acf31f84584
NEHA SUWAl <neha@jyaasa.com>
2015-02-13 13:14:02 +0545

    schema is updated after new columns added to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213053552) do
+ActiveRecord::Schema.define(version: 20150213062349) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20150213053552) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name",             limit: 255
+    t.string   "last_name",              limit: 255
+    t.boolean  "admin",                  limit: 1
+    t.datetime "login"
+    t.datetime "logout"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
