8810886a33385771123c6e3622f7d9492753ef2e
NEHA SUWAl <neha@jyaasa.com>
2015-02-13 16:10:44 +0545

    create action is removed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,5 @@ class HomeController < ApplicationController
     @user = current_user
   end
 
-  
-  private
-
-    def user_param
-      params.require(:users).permit(:first_name, :last_name, :login, :logout)
-    end
 
 end
