1646c13c6d4462d18328f45896e3afc0c2db08b3
NEHA SUWAl <neha@jyaasa.com>
2015-02-16 09:10:24 +0545

    migration is dropped and created again

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213114205) do
+ActiveRecord::Schema.define(version: 20150216032328) do
 
   create_table "records", force: :cascade do |t|
     t.integer  "user_id",     limit: 4
