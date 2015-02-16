e71b9dbe33f822639ae63d6d9bda583f5b18efce
NEHA SUWAl <neha@jyaasa.com>
2015-02-16 10:42:48 +0545

    modified index action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @user = current_user
+    @records = current_user.records
+    # @records = Record.where(:user_id => @user_id)
   end
 
 
