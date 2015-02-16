645aff88bffe60b583d2204a6e63a408a8149193
NEHA SUWAl <neha@jyaasa.com>
2015-02-16 15:47:28 +0545

    create action for when user clicks login button and update action when user clicks logout button is written

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -7,24 +7,25 @@ class RecordsController < ApplicationController
   end
 
   def create
-    binding.pry
     if current_user.records.where("login_time > ?",Time.now.beginning_of_day).empty?
-      @record = current_user.records.create(:login_time => Time.now)
+      @records = current_user.records.create(:login_time => Time.now)
       flash[:notice] = "Login time was added successfully."
     end
       @records = current_user.records
-      redirect_to(root_path)
+      @todays_record = current_user.records.where("login_time > ?",Time.now.beginning_of_day).first
   end
 
   def update
-    @records = Record.find(params[:id])
-    @todays_record = current_user.records.where("login_time > ?",Time.now.beginning_of_day).first
-    if @records.update_attributes(:logout_time => Time.now)
-      flash[:notice] = "Logout time was added successfully."
-      redirect_to(root_path( :user_id => @todays_record.id))
-    else
-      redirect_to(root_path)
+    @record = Record.find(params[:id])
+    unless @record.logout_time.present?
+      if @record.update_attributes(:logout_time => Time.now)
+        @records = current_user.records
+        flash[:notice] = "Logout time was added successfully."
+        @todays_record = current_user.records.where("login_time > ?",Time.now.beginning_of_day).first
     end
+      else
+        redirect_to(root_path)
+      end
   end
     
   def new
