52fd1c76058907ce8881ce4a2f4be153e1587c68
NEHA SUWAl <neha@jyaasa.com>
2015-02-16 10:48:34 +0545

    nested resources is removed

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :users do
-    resources :records
-  end
+  resources :users
+  resources :records
   # You can have the root of your site routed with "root"
    root 'home#index'
 
