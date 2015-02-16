d747724ce2a51619ecb509c16de5f7508239c188
NEHA SUWAl <neha@jyaasa.com>
2015-02-16 10:43:23 +0545

    modified index action and created create action

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,12 +1,21 @@
 class RecordsController < ApplicationController
-
+  before_action :authenticate_user!
   
   def index
+    @records = current_user.records
+    render "home/index"
   end
 
   def create
+    @record = current_user.records.create(:login_time => Time.now)
+    @records = current_user.records
   end
 
+  def logout
+    
+  end
+    
+  end
   def new
   end
 
