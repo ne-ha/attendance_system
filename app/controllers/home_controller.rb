ce2c8ef614089df9cda48d62c773cc4166548e20
NEHA SUWAl <neha@jyaasa.com>
2015-02-16 13:20:28 +0545

    defined instance varirable

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   
   def index
     @records = current_user.records
+    @todays_record = current_user.records.where("login_time > ?",Time.now.beginning_of_day).first
     # @records = Record.where(:user_id => @user_id)
   end
 
